0b1ec4b6e2729714625619a25e7f3338e99bf689
patricio-dsgn <patricio.ngg2@gmail.com>
2022-09-14 00:15:31 -0300

    ok

---
+++ src/js/big-text.js
@@ -0,0 +1,392 @@
+
+
+const colors = [
+    '#00f27e',
+    '#59f233',
+    '#9ed50c',
+    '#bfbb13',
+    '#d37300',
+    '#c83104',
+    '#ca0023',
+    '#c90080',
+    '#b800bf',
+    '#6a19c0',
+    '#341dc0',
+    '#2b50bf',
+    '#007abe',
+    '#00acbb',
+    '#00ccc6',
+]
+
+
+const abc = [
+    [
+        'ooooooo',
+        'oo   oo',
+        'ooooooo',
+        'oo   oo',
+        'oo   oo'
+    ],[
+        'oooooo ',
+        'oo   oo',
+        'oooooo ',
+        'oo   oo',
+        'oooooo '
+    ],[
+        'ooooooo',
+        'oo     ',
+        'oo     ',
+        'oo     ',
+        'ooooooo'
+    ],[
+        'oooooo  ',
+        'oo   oo ',
+        'oo    oo',
+        'oo   oo ',
+        'oooooo  '
+    ],[
+        'ooooooo',
+        'oo     ',
+        'oooooo ',
+        'oo     ',
+        'ooooooo'
+    ],[
+        'oooooo',
+        'oo    ',
+        'oooooo',
+        'oo    ',
+        'oo    '
+    ],[
+        'ooooooo',
+        'oo     ',
+        'ooooooo',
+        'oo   oo',
+        'ooooooo'
+    ],[
+        'oo   oo',
+        'oo   oo',
+        'ooooooo',
+        'oo   oo',
+        'oo   oo'
+    ],[
+        'ooo',
+        'ooo',
+        'ooo',
+        'ooo',
+        'ooo',
+    ],[
+        '     oo',
+        '     oo',
+        'oo   oo',
+        'oo   oo',
+        'ooooooo'
+    ],[
+        'oo  ooo',
+        'ooooo  ',
+        'oo     ',
+        'ooooo  ',
+        'oo  ooo'
+    ],[
+        'oo     ',
+        'oo     ',
+        'oo     ',
+        'oo     ',
+        'ooooooo'
+    ],[
+        'oo   oo',
+        'ooo ooo',
+        'ooooooo',
+        'oo   oo',
+        'oo   oo'
+    ],[
+        'oo   oo',
+        'ooo  oo',
+        'oo o oo',
+        'oo  ooo',
+        'oo   oo'
+    ],[
+        ' ooooo ',
+        'oo   oo',
+        'oo   oo',
+        'oo   oo',
+        ' ooooo '
+    ],[
+        'oooooo ',
+        'oo   oo',
+        'oooooo ',
+        'oo     ',
+        'oo     '
+    ],[
+        ' ooooo    ',
+        'oo   oo   ',
+        'oo   oo   ',
+        'oo   oooo ',
+        ' ooooo  oo'
+    ],[
+        'oooooo ',
+        'oo  ooo',
+        'oooooo ',
+        'oo  oo ',
+        'oo   oo'
+    ],[
+        ' oooooo',
+        'ooo    ',
+        ' ooooo ',
+        '    ooo',
+        'oooooo '
+    ],[
+        'ooooooo',
+        '  ooo  ',
+        '  ooo  ',
+        '  ooo  ',
+        '  ooo  '
+    ],[
+        'oo   oo',
+        'oo   oo',
+        'oo   oo',
+        'oo   oo',
+        'ooooooo'
+    ],[
+        'o     o',
+        'oo   oo',
+        ' oo oo ',
+        ' ooooo ',
+        '  ooo  '
+    ],[
+        'o     oo     o',
+        'oo   oooo   oo',
+        ' o   o  o   o ',
+        ' ooooo  ooooo ',
+        '  ooo    ooo  '
+    ],[
+        'oo   oo',
+        ' oo oo ',
+        '  ooo  ',
+        ' oo oo ',
+        'oo   oo'
+    ],[
+        'oo   oo',
+        'oo   oo',
+        ' oo oo ',
+        '  ooo  ',
+        '  ooo  '
+    ],[
+        'ooooooo',
+        '    ooo',
+        '  ooo  ',
+        'ooo    ',
+        'ooooooo'
+    ],[
+        'ooooo',
+        '  ooo',
+        '  ooo',
+        '  ooo',
+        '  ooo'
+    ],[
+        'ooooooo',
+        'oo   oo',
+        '   ooo ',
+        ' ooo   ',
+        'ooooooo'
+    ],[
+        'ooooooo',
+        '    ooo',
+        ' oooooo',
+        '    ooo',
+        'ooooooo'
+    ],[
+        'oo   oo',
+        'oo   oo',
+        'ooooooo',
+        '     oo',
+        '     oo'
+    ],[
+        'ooooooo',
+        'oo     ',
+        'ooooooo',
+        '     oo',
+        'ooooooo'
+    ],[
+        'ooooooo',
+        '     oo',
+        'ooooooo',
+        'oo   oo',
+        'ooooooo'
+    ],[
+        'ooooooo',
+        '    oo ',
+        '   oo  ',
+        ' oo    ',
+        'oo     '
+    ],[
+        'ooooooo',
+        'oo   oo',
+        'ooooooo',
+        'oo   oo',
+        'ooooooo'
+    ],[
+        'ooooooo',
+        'oo   oo',
+        'ooooooo',
+        '     oo',
+        'ooooooo'
+    ],[
+        'ooooooo',
+        'oo   oo',
+        'oo   oo',
+        'oo   oo',
+        'ooooooo'
+    ],[
+        '   ',
+        '   ',
+        '   ',
+        'ooo',
+        'ooo'
+    ],[
+        '     ',
+        '     ',
+        '     ',
+        'ooo  ',
+        'ooooo'
+    ],[
+        'ooo',
+        'ooo',
+        '   ',
+        'ooo',
+        'ooo'
+    ],[
+        'ooo  ',
+        'ooo  ',
+        '     ',
+        'ooo  ',
+        'ooooo'
+    ],[
+        '       ',
+        '       ',
+        '       ',
+        '       ',
+        'ooooooo'
+    ],[
+        '    ',
+        '    ',
+        'oooo',
+        '    ',
+        '    '
+    ],[
+        '    oo',
+        '   oo ',
+        '  oo  ',
+        ' oo   ',
+        'oo    '
+    ],[
+        '      ',
+        '  oo  ',
+        'oooooo',
+        'oooooo',
+        '  oo  '
+    ],[
+        '       ',
+        '       ',
+        '       ',
+        '       ',
+        '       '
+    ]
+];
+
+const order = [
+    'a','b','c','d','e','f','g','h','i',
+    'j','k','l','m','n','o','p','q','r',
+    's','t','u','v','w','x','y','z',
+    '1','2','3','4','5','6','7','8','9','0',
+    '.',',',':',';','_','-','/','+',' ',
+];
+
+
+
+const text = document.getElementById('text');
+const input = document.getElementById('in');
+
+input.focus();
+input.select();
+
+
+input.addEventListener('keyup',()=> magic(text))
+// input.on('keyup input',()=> magic(text))
+function magic(params) {
+    let textInput = input.value.toLowerCase();
+    let ti = big(textInput);
+    text.innerHTML = `<pre>\n\n${ti}\n\n</pre>`;
+
+    const num = Math.floor(Math.random()*colors.length);    
+    document.getElementsByTagName('body')[0].style.backgroundColor = colors[num];
+}
+
+
+
+function big(val) {
+
+    let h = abc[0].length;
+    let w = abc[0][1].length;
+    let s = 2;
+    let arr = val.split('');
+    let out = '';
+    for (let i = 0; i < h; i++) {
+        
+        arr.forEach((element,index) => {
+            let o = order.indexOf(element);
+            if(o !== -1){
+                let letter = abc[o][i];
+                out += letter+'  ';
+            }
+            
+        });
+        
+        out += '\n';
+    }
+
+    // return out;
+    return out
+}
+
+
+
+
+function copyToClipboard(elementId) {
+
+    // Create an auxiliary hidden input
+    var aux = document.createElement("input");
+  
+    // Get the text from the element passed into the input
+    aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+  
+    // Append the aux input to the body
+    document.body.appendChild(aux);
+  
+    // Highlight the content
+    aux.select();
+  
+    // Execute the copy command
+    document.execCommand("copy");
+  
+    // Remove the input from the body
+    document.body.removeChild(aux);
+  
+  }
+  
+  function log(){
+      console.log('---')
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
